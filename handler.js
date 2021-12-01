37ee6a80c9b0a04f9b9d52a3bf6cab246d085a75
angel.avila.flores <angel.avila.s200@gmail.com>
2021-12-01 10:39:53 -0600

    iniciando push

---
+++ handler.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'hello',
+        input: [],
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+
+module.exports.productos = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Listado de productos',
+        data:{
+          "nombre":"Angel Avila Flores",
+          "correo":"angel.af@xentra.mx",
+          "clave": 112
+        }
+      }
+    ),
+  };
+};
+
+module.exports.marcas = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Listado de marcas',
+        data:{
+          "nombre":"Angel Avila Flores",
+          "correo":"angel.af@xentra.mx",
+          "clave": 112
+        }
+      }
+    ),
+  };
+};
