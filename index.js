d234e5b14652813fd9da61ca10d2f2b9af8940ea
angel.avila.flores <angel.avila.s200@gmail.com>
2021-12-02 10:54:39 -0600

    correccion error 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const serverless = require('serverless-http');
 const express = require('express');
-const routerApi = require('./routes');
+//const routerApi = require('./routes');
 const {config} = require('./config/config');
 const {logErrors, errorHandler,boomErrorHandler, sqlErrorHamdler} = require('./middleware/errorHandler');
 const {validaKey} = require('./middleware/authHandler');
@@ -11,6 +11,10 @@ const app = express();
     //const port = config.port;
     app.use(express.json());
 
+
+    app.get('/producto', function (req, res) {
+        res.send('producto!')
+    })
     /*
     app.get('/productos', function (req, res) {
         res.send('get_productos!')
@@ -26,7 +30,7 @@ const app = express();
         res.send('get_marcas!')
     })*/
 
-    routerApi(app);
+    //routerApi(app);
     app.use(logErrors);
     app.use(sqlErrorHamdler);
     app.use(boomErrorHandler);
