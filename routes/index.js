735509517d2981f6a4c4bf3a84fbc55d905c2b2c
angel.avila.flores <angel.avila.s200@gmail.com>
2021-12-02 10:32:48 -0600

    correccion error 2

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ const Products = require('./productosRoutes');
 
 function routerApi(app){
 
-  const ruta_v1 = express.Router();
-  app.use('/v1',ruta_v1)
-  ruta_v1.use('/productos',Products)
+  const ruta = express.Router();
+  ruta.use('/productos',Products)
 
 }
 
