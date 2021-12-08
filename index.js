655e85d5040a2a523897f3d57aebacbda8f30ed3
angel.avila.flores <angel.avila.s200@gmail.com>
2021-12-08 15:59:08 -0600

    ajuste para cors con options 8

--- index.js
+++ index.js
@@ -94,13 +94,6 @@ const app = express();
       });
     });*/
 
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-      next();
-    });
-
 
     routerApi(app);
     app.use(logErrors);
